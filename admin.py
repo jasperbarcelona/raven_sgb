6d71d03f9cb323bb60a97e7f8e858df9095d1d35
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2015-03-15 03:30:14 +0800

    a

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 import flask, flask.views
-from flask import url_for, request, session, redirect
+from flask import url_for, request, session, redirect, jsonify, Response
 from jinja2 import environment, FileSystemLoader
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext import admin
@@ -10,6 +10,8 @@ from flask import render_template, request
 from flask import session, redirect
 from flask_oauth import OAuth
 from functools import wraps
+import json
+import datetime
 import time
 import os
 
@@ -21,8 +23,42 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 API_KEY = 'ecc67d28db284a2fb351d58fe18965f9'
 # os.environ['DATABASE_URL']
 
+now = datetime.datetime.now()
+MORNING_START = now.replace(hour=8, minute=0, second=0, microsecond=0)
+MORNING_END = now.replace(hour=12, minute=0, second=0, microsecond=0)
 
-class Log(db.Model):
+AFTERNOON_START = now.replace(hour=13, minute=0, second=0, microsecond=0)
+AFTERNOON_END = now.replace(hour=16, minute=0, second=0, microsecond=0)
+
+
+class Serializer(object):
+  __public__ = None
+
+  def to_serializable_dict(self):
+    dict = {}
+    for public_key in self.__public__:
+      value = getattr(self, public_key)
+      if value:
+        dict[public_key] = value
+    return dict
+
+
+class SWEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, Serializer):
+      return obj.to_serializable_dict()
+    if isinstance(obj, (datetime)):
+      return obj.isoformat()
+    return json.JSONEncoder.default(self, obj)
+
+
+def SWJsonify(*args, **kwargs):
+  return app.response_class(json.dumps(dict(*args, **kwargs), cls=SWEncoder, indent=None if request.is_xhr else 2), mimetype='application/json')
+  # from https://github.com/mitsuhiko/flask/blob/master/flask/helpers.py
+
+
+class Log(db.Model, Serializer):
+    __public__ = ['id','date','id_no','name','section','time_in','time_out','timestamp']
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String(20))
     id_no = db.Column(db.String(20))
@@ -34,29 +70,57 @@ class Log(db.Model):
     timestamp = db.Column(db.String(20))
 
 
+class Late(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(20))
+    id_no = db.Column(db.String(20))
+    time_in = db.Column(db.String(10))
+
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60))
+    level = db.Column(db.String(10))
+    section = db.Column(db.String(30))
+    department = db.Column(db.String(20))
+
+class IngAdmin(sqla.ModelView):
+    column_display_pk = True
+admin = Admin(app)
+admin.add_view(IngAdmin(Student, db.session))
+admin.add_view(IngAdmin(Late, db.session))
+
+
+def get_hour(time):
+    if time[6:8] == 'PM' and time[:2] != '12':
+        hour = int(time[:2]) + 12
+        print hour
+        return hour
+    hour = int(time[:2])
+    print hour
+    return hour
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    log = Log.query.order_by(Log.timestamp.desc())
-    return flask.render_template(
-        'index.html',
-        date=time.strftime("%B %d, %Y"),
-        log=log
-        )
+    a = Log.query.filter_by().order_by(Log.timestamp.desc()).all()
+    return SWJsonify({'Logs': a})
 
 
 @app.route('/addlog', methods=['GET', 'POST'])
 def add_log():
     api_key = flask.request.form.get('api_key')
 
-    if not api_key or api_key != API_KEY:
-        return 'Invalid API Key!'
+    # if not api_key or api_key != API_KEY:
+    #     return 'Invalid API Key!'
 
-    id_no = flask.request.form.get('id_no')
-    name = flask.request.form.get('name')
-    level = flask.request.form.get('level')
-    section = flask.request.form.get('section')
-    date = flask.request.form.get('date')
-    time_in = flask.request.form.get('time_in')
+    id_no = flask.request.args.get('id_no')
+    name = flask.request.args.get('name')
+    level = flask.request.args.get('level')
+    section = flask.request.args.get('section')
+    date = flask.request.args.get('date')
+    time_in = flask.request.args.get('time_in')
 
     add_this = Log(
             date=date,
@@ -65,30 +129,44 @@ def add_log():
             level=level,
             section=section,
             time_in=time_in,
-            timestamp=time.strftime('%Y-%m-%d %H:%M:%S')
+            time_out='None',
+            timestamp=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S:%f')
             )
     
     db.session.add(add_this)
     db.session.commit()
 
-    return 'ok'
+    time_now = now.replace(hour=get_hour(time_in), minute=int(time_in[3:5]))
+
+    if (time_now >= MORNING_START and time_now < MORNING_END) or (time_now > AFTERNOON_START and time_now < AFTERNOON_END):
+        late = Late(date=date,id_no=id_no,time_in=time_in)
+        db.session.add(late)
+        db.session.commit()
+
+    return SWJsonify({
+        'Status': 'Logged In',
+        'Log': Log.query.all()
+        })
 
 
 @app.route('/timeout', methods=['GET', 'POST'])
 def time_out():
     api_key = flask.request.form.get('api_key')
 
-    if not api_key or api_key != API_KEY:
-        return 'Invalid API Key!'
+    # if not api_key or api_key != API_KEY:
+    #     return 'Invalid API Key!'
 
-    id_no = flask.request.form.get('id_no')
-    time_out = flask.request.form.get('time_out')
+    id_no = flask.request.args.get('id_no')
+    time_out = flask.request.args.get('time_out')
 
     a = Log.query.filter_by(id_no=id_no).order_by(Log.timestamp.desc()).first()
     a.time_out=time_out  
     db.session.commit()
 
-    return 'ok'
+    return SWJsonify({'Status': 'Logged Out',
+                      'Log': Log.query.filter_by(id_no=id_no)\
+                      .order_by(Log.timestamp.desc()).first()
+                      })
 
 
 @app.route('/db/rebuild', methods=['GET', 'POST'])
@@ -98,7 +176,7 @@ def rebuild_database():
 
     db.session.commit()
 
-    return 'ok'
+    return SWJsonify({'Status': 'Database Rebuild Success'})
 
 
 if __name__ == '__main__':
