06fc0e94bae0930da1dc8ec6ee85da291470da41
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2015-03-15 19:51:19 +0800

    a

--- admin.py
+++ admin.py
@@ -76,7 +76,7 @@ class Log(db.Model, Serializer):
     __public__ = ['id','school_id','date','id_no','name','level',
                   'section','time_in','time_out','timestamp']
 
-    id = db.Column(db.Column(db.String(50), primary_key=True, autoincrement=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     school_id = db.Column(db.Integer)
     date = db.Column(db.String(20))
     id_no = db.Column(db.String(20))
@@ -238,7 +238,7 @@ def add_log():
     time_in = flask.request.form.get('time_in')
 
     add_this = Log(
-            id=int(datetime.datetime.now().strftime('%Y%m%d%H%M%S')),
+            id=123456,
             school_id=school_id,
             date=date,
             id_no=id_no,
