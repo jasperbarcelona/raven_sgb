c71d83ca6bef0a152064abfb17f1d2e51082ae57
jasperbarcelona <barcelona.jasperoliver@gmail.com>
2015-03-17 04:05:39 +0800

    a

--- static/js/animations.js
+++ static/js/animations.js
@@ -3,5 +3,9 @@ $(document).ready(function(){
 var height = $(window).height()-110;
 $("#main-content").css("height",height);
 
+$(window).resize(function(){
+	var height = $(window).height()-110;
+	$("#main-content").css("height",height);
+});
 
 });
