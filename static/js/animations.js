cf378fd737746f7b71a4a3d2b30ce3c505c3d480
sepsallao <jasper.barcelona@infoshiftinc.com>
2015-04-04 02:42:14 +0800

    a

--- static/js/animations.js
+++ static/js/animations.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
-var height = $(window).height()-110;
+var height = $(window).height()-118;
 $("#main-content").css("height",height);
 
 $(window).resize(function(){
-	var height = $(window).height()-110;
+	var height = $(window).height()-118;
 	$("#main-content").css("height",height);
 });
 
