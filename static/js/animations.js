c24c165a9cd3d6a6d64872a09df56f0651194034
sepsallao <jasper.barcelona@infoshiftinc.com>
2015-04-04 12:51:47 +0800

    A

--- static/js/animations.js
+++ static/js/animations.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
+var page = $('#main-content');  // set to the main content of the page   
+    $(window).mousewheel(function(event, delta, deltaX, deltaY){
+        if (delta < 0) page.scrollTop(page.scrollTop() + 65);
+        else if (delta > 0) page.scrollTop(page.scrollTop() - 65);
+        return false;
+    })
+
 var height = $(window).height()-118;
 $("#main-content").css("height",height);
 
