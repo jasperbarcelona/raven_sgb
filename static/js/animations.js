7d2c4e441d935dd2037b282620c64dbca463dc42
sepsallao <jasper.barcelona@infoshiftinc.com>
2015-04-04 13:54:20 +0800

    A

--- static/js/animations.js
+++ static/js/animations.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
-var height = $(window).height()-118;
+var height = $(window).height()-112;
 $("#main-content").css("height",height);
 
 $(window).resize(function(){
-	var height = $(window).height()-118;
+	var height = $(window).height()-112;
 	$("#main-content").css("height",height);
 });
 
