00c29ab50156d816462522314974e78c89ba8246
sepsallao <jasper.barcelona@infoshiftinc.com>
2015-04-11 11:02:26 +0800

    A

--- admin.py
+++ admin.py
@@ -529,51 +529,51 @@ def rebuild_database():
         )
     db.session.add(school)
 
-    for i in range(100):
-        a = Student(
-            school_id=1234,
-            id_no='2011334281',
-            first_name='Jasper',
-            last_name='Barcelona',
-            middle_name='Estrada',
-            level='2nd Grade',
-            department='student',
-            section='Fidelity',
-            absences='0',
-            lates='0',
-            parent_contact='639183339068'
-            )
 
-        c = Student(
-            school_id=1234,
-            id_no='2011334281',
-            first_name='Janno',
-            last_name='Armamento',
-            middle_name='Estrada',
-            level='2nd Grade',
-            department='student',
-            section='Fidelity',
-            absences='0',
-            lates='0',
-            parent_contact='639183339068'
-            )
+    a = Student(
+        school_id=1234,
+        id_no='2011334281',
+        first_name='Jasper',
+        last_name='Barcelona',
+        middle_name='Estrada',
+        level='2nd Grade',
+        department='student',
+        section='Fidelity',
+        absences='0',
+        lates='0',
+        parent_contact='639183339068'
+        )
 
-        b = Student(
-            school_id=1234,
-            id_no='2011334282',
-            first_name='Prof',
-            last_name='Barcelona',
-            middle_name='Estrada',
-            department='faculty',
-            absences='0',
-            lates='0',
-            parent_contact='639183339068'
-            )
+    c = Student(
+        school_id=1234,
+        id_no='2011334282',
+        first_name='Janno',
+        last_name='Armamento',
+        middle_name='Estrada',
+        level='2nd Grade',
+        department='student',
+        section='Fidelity',
+        absences='0',
+        lates='0',
+        parent_contact='639183339068'
+        )
 
-        
-        db.session.add(a)
-        db.session.add(b)
-        db.session.add(c)
+    b = Student(
+        school_id=1234,
+        id_no='2011334234',
+        first_name='Prof',
+        last_name='Barcelona',
+        middle_name='Estrada',
+        department='faculty',
+        absences='0',
+        lates='0',
+        parent_contact='639183339068'
+        )
+
+    
+    db.session.add(a)
+    db.session.add(b)
+    db.session.add(c)
     db.session.commit()
 
     return SWJsonify({'Status': 'Database Rebuild Success'})
